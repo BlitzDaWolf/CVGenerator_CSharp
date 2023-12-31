ac6798cd247425a2d7ec61806684da487a08dc87
BlitzDaWolf <botjes2@hotmail.com>
2023-12-31 16:45:54 +0100

    Init

---
+++ CVGenerator_CSharp/CVDocument.cs
@@ -0,0 +1,16 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
+
+namespace CVGenerator_CSharp
+{
+    public class CVDocument : IDocument
+    {
+        public void Compose(IDocumentContainer container)
+        {
+            container.Page(page =>
+            {
+
+            });
+        }
+    }
+}
--- CVGenerator_CSharp/Program.cs
+++ CVGenerator_CSharp/Program.cs
@@ -1,4 +1,24 @@
 ﻿using CVGenerator_CSharp;
+using QuestPDF.Fluent;
+using QuestPDF.Previewer;
 
 GitHub gitHub = new GitHub();
 
+CVDocument document = new CVDocument();
+document.ShowInPreviewer();
+
+/*Document.Create(container =>
+{
+    container.Page(page =>
+    {
+        page.Header()
+            .AlignCenter()
+            .Text("Test")
+            .FontSize(30);
+
+        page.Content()
+            .PaddingHorizontal(25)
+            .BorderHorizontal(1);
+    });
+})
+.ShowInPreviewer();*/
