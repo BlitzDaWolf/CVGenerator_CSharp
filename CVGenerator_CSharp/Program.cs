134cc8c7d90139afdbee8901bdcb1f13b7d9c69b
BlitzDaWolf <botjes2@hotmail.com>
2023-12-31 23:12:51 +0100

    Fixed licence type

--- CVGenerator_CSharp/Program.cs
+++ CVGenerator_CSharp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using CVGenerator_CSharp;
+using QuestPDF;
+using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
 using QuestPDF.Previewer;
 
 var pdetail = Newtonsoft.Json.JsonConvert.DeserializeObject<PersonDetail>(File.ReadAllText("./PersonDetails.json"));
@@ -6,6 +9,8 @@ var pdetail = Newtonsoft.Json.JsonConvert.DeserializeObject<PersonDetail>(File.R
 pdetail.Expierences = pdetail.Expierences.OrderByDescending(x => x.StartDate).ToList();
 pdetail.Education = pdetail.Education.OrderByDescending(x => x.StartDate).ToList();
 
+Settings.License = LicenseType.Community;
+
 CVDocument document = new CVDocument(pdetail.GitHub, pdetail);
 document
-    .ShowInPreviewer();
+    .GeneratePdf($"./CV-{pdetail.FullName}.pdf");
